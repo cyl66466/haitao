2d74ec18799bf50a419a82f8fe4528e141986d3b
cyl66466 <13058019981@163.com>
2017-09-25 17:14:34 +0800

    OK

--- demo/src/main.js
+++ demo/src/main.js
@@ -6,15 +6,6 @@ Vue.use(VueRouter);
 
 import routes from './router/router.config.js';
 import store from './store/index.js';
-// import './assets/css/base.css';
-// import './assets/css/mui.min.css';
-// import './assets/css/loaders.min.css';
-// import './assets/css/loading.css';
-// import './assets/css/swiper.min.css';
-// import './assets/js/rem.js';
-// import './assets/js/jquery.min.js';
-// import './assets/js/swiper.jquery.min.js';
-// import 'font-awesome-webpack';
 const router = new VueRouter({
 	routes
 })
